1c88263363085e1b93fb423bc0ce853acb11321e
TradesByEli <elijahmontalvo00@gmail.com>
2026-01-05 10:35:42 -0500

    New Changes

--- PositionCalculator.py
+++ PositionCalculator.py
@@ -5,17 +5,21 @@ st.title("Position Size Calculator")
 
 # Percent multipliers
 percent_multiplier = {
-    "1% (D)": 0.01,
-    "5% (C)": 0.05,
-    "7.5% (C+)": 0.075,
-    "10% (B-)": 0.10,
-    "12.5% (B)": 0.125,
-    "15% (B+)": 0.15,
-    "30% (A-)": 0.30,
+    "1%": 0.01,
+    "2%": 0.02,
+    "5% (B-)": 0.05,
+    "7.5%": 0.075,
+    "10% (B)": 0.10,
+    "12.5%": 0.125,
+    "15%": 0.15,
+    "20% (B+)": 0.20,
+    "25%": 0.25,
+    "30% (A-)":0.30,
+    "40%": 0.40,
     "50% (A)": 0.50,
-    "80% (A+)": 0.80,
-    "90% (A++)": 0.90,
-    "100% (A+++)": 1,
+    "80%": 0.80,
+    "90%": 0.90,
+    "100% (A+)": 1,
 
 }
 
@@ -37,6 +41,6 @@ if daily_stop > 0 and stop_loss_share > 0 and percent_of_DS != "-":
     allotted_risk = daily_stop * percent_multiplier[percent_of_DS]
     position_size = allotted_risk / stop_loss_share
 
-    st.subheader(f"Position Size: {int(position_size)} shares " + (f"(${allotted_risk:.2f} risk)"))
+    st.subheader(f"Position Size: {int(position_size)} shares " + f"(${allotted_risk:.2f} risk)")
 else:
     st.info("Enter: Daily stop, Percent, and Stop loss/share to calculate position size")
