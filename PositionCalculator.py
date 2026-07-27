b1dced53f08388209272e04ea0d4fc25c0b4b3de
Elijah <elijahmontalvo00@gmail.com>
2026-07-27 10:38:44 -0400

    Change default daily stop value from 150 to 200

--- PositionCalculator.py
+++ PositionCalculator.py
@@ -28,7 +28,7 @@ with input_cols[0]:
     daily_stop = st.number_input(
         "Daily Stop ($)",
         min_value=0.0,
-        value=150.0,
+        value=200.0,
         step=10.0,
         format="%.2f",
         help="Max amount you allow yourself to lose today.",
